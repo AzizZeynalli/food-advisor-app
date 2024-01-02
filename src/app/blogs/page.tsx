4b58c0cbdf17fcf595e4c6fa3a616e6b24c47cc6
AzizZeynalli <a.zeynalli1@ufaz.az>
2024-01-02 19:01:02 +0400

    update

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,12 +1,12 @@
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Page() {
-    return (
-        <div>
-        <h1>Blogs Page</h1>
-        <Header />
-        <Footer />
-        </div>
-    );
+  return (
+    <>
+      <Header />
+        
+      <Footer />
+    </>
+  );
 }
