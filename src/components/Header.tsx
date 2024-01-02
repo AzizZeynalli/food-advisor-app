5e6f03c6148b43897b5d20434366bcc8040abb85
Sama Zeynalli <sema.kazimli@gmail.com>
2024-01-03 00:35:35 +0400

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,32 @@
+import { Box, HStack, Image, Button, Text} from "@chakra-ui/react";
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <div>
-        <h1>Header</h1>
-        </div>
+        <HStack p='20px' justifyContent='space-between' w='100%' maxH='64px' bg='blue.50'>
+            <HStack gap='16px'>
+                <Image src="../images/logo.svg" />
+                <HStack gap='16px' pt='8px' fontSize='16px' fontWeight='500px' color='gray.400'>
+                    <Link href={"/blog"} passHref>
+                        <Button as="a" variant="link" _hover={{ color: 'gray.600' }}>Blog</Button>
+                    </Link>
+                    <Link href={"/recipes"} passHref>
+                        <Button as="a" variant="link" _hover={{ color: 'gray.600' }}>Recipes</Button>
+                    </Link>
+                    <Link href={"/help"} passHref>
+                        <Button as="a" variant="link" _hover={{ color: 'gray.600' }}>Help</Button>
+                    </Link>
+                </HStack>
+            </HStack>
+            <HStack gap='16px'>
+                <HStack bg='white' borderRadius="4px" fontSize='16px' fontWeight='500px'>
+                    <Link href={"/login"}>
+                        <Button as="a" variant="link" _hover={{ color: 'gray.600' }} p='8px 16px'>Already have an account? Log in</Button>
+                    </Link>
+                    <Button p='8px 16px' borderRadius="500px" color='white' bg='gray.600' _hover={{ color: 'gray.600', backgroundColor: "white"}} ><Link href={"/signup"}>Sign up</Link></Button>
+                </HStack>
+                <Text>en</Text>
+            </HStack>
+        </HStack>
     );
 }
