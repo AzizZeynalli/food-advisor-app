a39142e2f039d88cde6e7fcf6a5bddb3cee5f503
R3hm4n <eliyev.99.18@gmail.com>
2024-01-14 22:33:20 +0400

    adding help page

---
+++ src/app/help/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+import axios from "axios";
+import { VStack, Image, Text, useToast } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import PasswordInput from "@/components/(formComponents)/PasswordInput";
+import EmailInput from "@/components/(formComponents)/EmailInput";
+import LoginButton from "@/components/(formComponents)/LoginButton";
+import HelpTitle from "@/components/helpPage/HelpTitle";
+import HelpTextarea from "@/components/helpPage/HelpTextarea";
+import HelpButton from "@/components/helpPage/HelpButton";
+
+const HelpPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [isloading, setIsLoading] = useState(false);
+  const [isErr, setIsErr] = useState(false);
+
+  const toast = useToast();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (window) {
+      window.history.pushState(null, "", window.location.href);
+      window.onpopstate = function () {
+        window.history.go(1);
+      };
+    }
+  }, []);
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const {
+    control,
+    formState: { errors, isValid },
+    handleSubmit,
+    getValues,
+  } = useForm({
+    mode: "all",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = (data: any) => {
+    console.log(data);
+    toast({
+        title: "We received your application. Please wait for our response!",
+        status: "success",
+        duration: 3000,
+        position: "top",
+        isClosable: true,
+        colorScheme: "blue"
+    })
+  };
+  return (
+    <VStack
+      gap="0"
+      justifyContent="center"
+      alignItems="center"
+      minWidth="400px"
+      height="100vh"
+      padding="50px"
+      shadow="sm"
+        borderWidth={3}
+        borderRadius="lg"
+    >
+      <Image
+        src="../images/Logo.svg"
+        cursor="pointer"
+        width={{ base: "40%", sm: "35%", md: "30%", lg: "20%" }}
+        mb={{ base: "20px", lg: "40px" }}
+        alt=""
+        onClick={() => {
+          router.push("/");
+        }}
+      />
+      <Text alignSelf="flex-start" pl="10%" pb="20px" fontSize="20px" fontWeight="700">If you have any problem, please write us!</Text>
+      <VStack width="80%">
+        <EmailInput errors={errors} control={control} />
+        <HelpTitle errors={errors} control={control} />
+        <HelpTextarea errors={errors} control={control} />
+        <HelpButton
+          handleSubmit={handleSubmit}
+          onSubmit={onSubmit}
+          isValid={isValid}
+          isloading={isloading}
+        />
+      </VStack>
+    </VStack>
+  );
+};
+export default HelpPage;
---
+++ src/components/helpPage/HelpButton.tsx
@@ -0,0 +1,31 @@
+import { Button, Spinner,Text, VStack } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import React from 'react'
+ interface IHelpButtonProps {
+    handleSubmit: any,
+    onSubmit: any,
+    isValid: any,
+    isloading: boolean
+ }
+const HelpButton: React.FC<IHelpButtonProps> = ({handleSubmit,onSubmit, isloading,isValid}) => {
+    const router = useRouter();
+  return (
+    
+    <VStack width="100%">
+     <Button
+          mb="20px"
+          mt="20px"
+          width="100%"
+          colorScheme="blue"
+          variant="solid"
+          color="white"
+          onClick={handleSubmit(onSubmit)}
+          isDisabled={!isValid}
+        >
+          {isloading ? <Spinner /> : "Send"}
+        </Button>
+    </VStack>
+  )
+}
+
+export default HelpButton
