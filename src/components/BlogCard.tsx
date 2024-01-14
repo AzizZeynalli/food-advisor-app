77aa4497aae04748c65da781402accff9f16090c
AzizZeynalli <a.zeynalli1@ufaz.az>
2024-01-14 18:59:01 +0400

    updated

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -8,13 +8,14 @@ import {
   Stack,
   Text,
   CardFooter,
-  Divider,
   Button,
   Box,
   HStack,
   Link,
   Flex,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { AiOutlineLike } from "react-icons/ai";
 
 type TBlog = {
@@ -23,29 +24,61 @@ type TBlog = {
   content: string;
   likes: number;
   image: string;
+  dateCreated: string;
   user: {
-    id: string;
-    name: string;
-  };
+    username: string;
+    email: string;
+  }
 };
 
 type TBlogCard = {
   blog: TBlog;
 };
 
-export default function BlogCard({ blog }: TBlogCard) {
-  const handleLike = () => {
-    alert("You liked this blog");
+const BlogCard = ({ blog }: TBlogCard) => {
+  useEffect(() => {
+    // Consider handling token storage and retrieval more securely
+    localStorage.setItem("token", "");
+  }, []);
+
+  const [likes, setLikes] = useState(blog.likes);
+
+  const updateLikes = async () => {
+    try {
+      const response = await axios.patch(
+        `http://localhost:3003/api/blogs/${blog.id}/like`,
+        {}, // Empty object for the request body
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      console.log(response.data); // Log or handle the response data if needed
+    } catch (error) {
+      console.error(error);
+      throw error; // Rethrow the error to handle it in the calling function if needed
+    }
   };
+
+  const handleLike = async () => {
+    try {
+      setLikes((prev) => prev + 1);
+      await updateLikes();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <Card maxW="xl" h="xl" rounded="xl">
+    <Card w="md" h="xl" rounded="xl" shadow="md">
       <CardHeader>
-        <Flex gap="4">
+        <Flex gap="2">
           <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
             <Box>
-              <Heading size="sm">{blog.title}</Heading>
-              <Text>{blog.title}</Text>
+              <Heading size="sm">{blog.user.username}</Heading>
             </Box>
+            {blog.dateCreated && <Text fontSize="sm">{blog.dateCreated}</Text>}
           </Flex>
         </Flex>
       </CardHeader>
@@ -53,20 +86,22 @@ export default function BlogCard({ blog }: TBlogCard) {
         <Stack mt="2" spacing="3">
           <Heading size="md">{blog.title}</Heading>
           <Text>{blog.content}</Text>
-          <Link color="blue.600" fontSize="lg">
+          <Link color="blue.600" fontSize="lg" mt={3}>
             Read more...
           </Link>
         </Stack>
       </CardBody>
-      <Image
-        src={`data:image/jpg;base64,${blog.image}`}
-        alt="blog image"
-        width="full"
-        height={200}
-        mx="auto"
-        objectFit="cover"
-        objectPosition="top"
-      />
+      {blog.image && (
+        <Image
+          src={`data:image/jpg;base64,${blog.image}`}
+          alt="blog image"
+          width="full"
+          height={250}
+          mx="auto"
+          objectFit="cover"
+          objectPosition="top"
+        />
+      )}
       <CardFooter justify="center">
         <HStack spacing="2">
           <Button
@@ -79,10 +114,12 @@ export default function BlogCard({ blog }: TBlogCard) {
             Like
           </Button>
           <Text fontSize="lg" color="blue.500" fontWeight="semibold">
-            {blog.likes} likes
+            {likes} likes
           </Text>
         </HStack>
       </CardFooter>
     </Card>
   );
-}
+};
+
+export default BlogCard;
