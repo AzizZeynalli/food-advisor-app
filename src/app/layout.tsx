d1fbdede94e3e7b9521bd263a52ba9a4fdefb723
Murad Israfilov <muradisrafilov542@gmail.com>
2024-01-22 15:41:55 +0400

    Update images paths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,17 +12,17 @@ export const metadata: Metadata = {
   description: 'Your personal food advisor',
   icons: {
     icon: [
-      { url: 'images/favicon.png' },
+      { url: '/images/favicon.png' },
     ],
-    shortcut: ['images/favicon.png'],
+    shortcut: ['/images/favicon.png'],
     apple: [
-      { url: 'images/favicon.png' },
-      { url: 'images/favicon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/images/favicon.png' },
+      { url: '/images/favicon.png', sizes: '180x180', type: 'image/png' },
     ],
     other: [
       {
         rel: 'apple-touch-icon-precomposed',
-        url: 'images/favicon.png',
+        url: '/images/favicon.png',
       },
     ],
   },
